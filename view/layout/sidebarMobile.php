36cfa0120f9b2e537b50cb3c9adeaf0944e87542
nam216 <nguyennhatnam27082005@gmail.com>
2025-09-16 11:24:27 +0700

    chỉnh icon cho mobile

--- view/layout/sidebarMobile.php
+++ view/layout/sidebarMobile.php
@@ -1,109 +1,51 @@
 <style>
-    ul li.active svg,
-    ul li.active i {
+    /* Active item chung cho cả desktop + mobile */
+    nav ul li.active a,
+    nav ul li.active svg,
+    nav ul li.active i {
         color: red;
+    }
+
+    nav ul li.active i,
+    nav ul li.active svg {
         border: 2px solid red;
         border-radius: 6px;
         padding: 2px;
         display: inline-block;
     }
 
-    ul li svg,
-    ul li i {
+    /* Reset mặc định icon */
+    nav ul li i,
+    nav ul li svg {
         border: none;
         padding: 0;
         color: inherit;
     }
+
+    .sidebar-mobile ul li a {
+        display: flex;
+        align-items: center;
+        gap: 6px;
+        padding: 8px 12px;
+        text-decoration: none;
+        color: #333;
+    }
+
+    .sidebar-mobile ul li.active a {
+        color: red;
+        font-weight: 600;
+    }
+
+    .sidebar-mobile ul li.section-title {
+        font-size: 12px;
+        font-weight: bold;
+        color: #777;
+        margin-top: 10px;
+        padding: 6px 12px;
+    }
 </style>
 
-<div class="collapse navbar-collapse m-menu-i" id="navbarNav5">
-
-    <ul class="">
-        <li class="item"><svg class="home-icon" rpl="" fill="currentColor" height="20" icon-name="home-outline"
-                viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
-                <!--?lit$03863286$-->
-                <!--?lit$03863286$-->
-                <path
-                    d="m17.71 8.549 1.244.832v8.523a1.05 1.05 0 0 1-1.052 1.046H12.73a.707.707 0 0 1-.708-.707v-4.507c0-.76-1.142-1.474-2.026-1.474-.884 0-2.026.714-2.026 1.474v4.507a.71.71 0 0 1-.703.707H2.098a1.046 1.046 0 0 1-1.052-1.043V9.381l1.244-.835v9.158h4.44v-3.968c0-1.533 1.758-2.72 3.27-2.72s3.27 1.187 3.27 2.72v3.968h4.44V8.549Zm2.04-1.784L10.646.655a1.12 1.12 0 0 0-1.28-.008L.25 6.765l.696 1.036L10 1.721l9.054 6.08.696-1.036Z">
-                </path>
-                <!--?-->
-            </svg>
-            <a href="index.html" title="Trang chủ">Trang chủ</a>
-        </li>
-        <li class="item">
-            <i class="bi bi-newspaper idiscuss"></i>
-            <a href="tin-moi.html" title="Trang chủ">Mới nhất</a>
-        </li>
-        <li class="item">
-            <i class="bi bi-box-arrow-up-right trend-icon"></i>
-            <a href="trends.html" title="Trang chủ">Xu hướng</a>
-        </li>
-        <li class="item">
-            <i class="bi bi-chat-left-text idiscuss"></i>
-            <a href="binh-luan.html" title="Trang chủ">Bình luận</a>
-        </li>
-
-    </ul>
-    <div class="line"></div>
-
-    <label class="bg-tranparent">CHỦ ĐỀ</label>
-    <ul class=" top-item">
-
-        <li><img src="../img.dff.vn/Image/2024/07/17/ktvm-104446377.png"><a title="Vĩ mô" href="vi-mo-t128-1.html">
-                Vĩ mô</a> </li>
-
-        <li><img src="../img.dff.vn/Image/2024/07/17/ck-153008757.png"><a title="Thị trường"
-                href="thi-truong-t129-1.html"> Thị trường</a> </li>
-
-        <li><img src="../img.dff.vn/Image/2024/08/12/2-12045699.png"><a title="Crypto " href="crypto-t2202-1.html">
-                Crypto </a> </li>
-
-        <li><img src="../img.dff.vn/Image/2024/07/17/dn-104202856.png"><a title="360° Doanh nghiệp"
-                href="360-doanh-nghiep-t123-1.html"> 360° Doanh nghiệp</a> </li>
-
-        <li><img src="../img.dff.vn/Image/2024/07/17/nh-154022179.png"><a title="Tài chính"
-                href="tai-chinh-t139-1.html"> Tài chính</a> </li>
-
-        <li><img src="../img.dff.vn/Image/2024/07/17/bds-153325898.png"><a title="Nhà đất" href="nha-dat-t140-1.html">
-                Nhà đất</a> </li>
-
-        <li><img src="../img.dff.vn/Image/2024/08/12/1-120418263.png"><a title="Quốc tế " href="quoc-te-t2201-1.html">
-                Quốc tế </a> </li>
-
-        <li><img src="../img.dff.vn/Image/2024/08/12/3-120542176.png"><a title="Thảo luận"
-                href="thao-luan-t2203-1.html"> Thảo luận</a> </li>
-
-    </ul>
-
-    <div class="line"></div>
-    <ul class=" about-c">
-        <li><i class="bi bi-tv"></i>
-            <a href="policy.html#about"> Về chúng tôi</a>
-        </li>
-        <li>
-            <i class="bi bi-book"></i>
-            <a href="policy.html#terms-of-service"> Chính sách nội dung</a>
-        </li>
-        <li><svg rpl="" fill="currentColor" height="20" icon-name="topic-law-outline" viewBox="0 0 20 20" width="20"
-                xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M2.3 8.625 3.621 5.31l1.324 3.315h1.346L4.256 3.53a1.37 1.37 0 0 1 1.362-1.28h8.764a1.37 1.37 0 0 1 1.362 1.28l-2.035 5.1h1.346l1.324-3.32L17.7 8.625h1.346l-2.061-5.16A2.62 2.62 0 0 0 14.382 1H5.618a2.62 2.62 0 0 0-2.606 2.465L.951 8.625H2.3Z">
-                </path>
-                <path
-                    d="M6.617 10H.625a.625.625 0 0 0-.625.625 3.62 3.62 0 1 0 7.242 0A.625.625 0 0 0 6.617 10Zm-3 3a2.376 2.376 0 0 1-2.288-1.75h4.58A2.376 2.376 0 0 1 3.621 13h-.004Z">
-                </path>
-                <path
-                    d="M19.375 10h-5.992a.624.624 0 0 0-.625.625 3.622 3.622 0 0 0 6.966 1.386c.182-.44.276-.91.276-1.386a.624.624 0 0 0-.625-.625Zm-3 3a2.376 2.376 0 0 1-2.288-1.75h4.576A2.375 2.375 0 0 1 16.379 13h-.004Z">
-                </path>
-                <path d="M10.625 5h-1.25v12.7H6.479v1.25h7.042V17.7h-2.896V5Z"></path>
-            </svg>
-            <a href="policy.html#privacy-policy"> Chính sách riêng tư</a>
-        </li>
-        <li><i class="bi bi-badge-ad"></i>
-            <a href="policy.html#advertisement"> Quảng cáo </a>
-        </li>
-    </ul>
-</div>
+<!-- menu mobile -->
 <nav class="sidebar-mobile">
     <ul class="menu">
         <li class="active"><a href="/"><i class="fas fa-home icon"></i> <span>Trang chủ</span></a></li>
@@ -129,14 +71,13 @@
     </ul>
 </nav>
 
-</div>
 <script>
     document.addEventListener("DOMContentLoaded", function() {
-        const allLis = document.querySelectorAll("ul li");
+        const allLis = document.querySelectorAll(".sidebar-mobile ul li");
         allLis.forEach(li => {
             const a = li.querySelector("a");
             if (a) {
-                a.addEventListener("click", function(e) {
+                a.addEventListener("click", function() {
                     allLis.forEach(item => item.classList.remove("active"));
                     li.classList.add("active");
                 });
