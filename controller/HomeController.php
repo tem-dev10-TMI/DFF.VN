b443416ea556719e4337d05b33ce3302f1e68b7c
quocanh0381 <quacanh900@gmail.com>
2025-09-16 21:52:30 +0700

    hoàn thành rssrss

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -43,41 +43,41 @@ class homeController
         // RSS Báo Chính phủ
         $feedUrl1 = "https://baochinhphu.vn/kinh-te.rss";
         $rssArticles1 = RssModel::getFeedItems($feedUrl1, 50, 15);
+        foreach ($rssArticles1 as &$art) {
+            $art['author_id']   = 66;
+            $art['author_name'] = "Báo Chính Phủ";
+            $art['avatar_url']  = 'public/img/avatar/baochinhphu.png';
+        }
+        unset($art);
 
         // RSS Thanh Niên
         $feedUrl2 = "https://thanhnien.vn/rss/kinh-te.rss";
         $rssArticles2 = RssModel::getFeedItems($feedUrl2, 50, 15);
-
-        // Gộp RSS + DB articles
-        $articles = array_merge($rssArticles1, $rssArticles2, $dbArticles);
-
-        // Thiết lập avatar riêng theo nguồn RSS
-        foreach ($articles as &$art) {
-            if (!empty($art['is_rss'])) {
-                if (isset($art['link']) && str_contains($art['link'], 'thanhnien')) {
-                    $art['avatar_url'] = 'public/img/avatar/thanhnien.png';
-                } else {
-                    $art['avatar_url'] = 'public/img/avatar/baochinhphu.png';
-                }
-                $art['author_id'] = 66; // id mặc định cho RSS
-            }
+        foreach ($rssArticles2 as &$art) {
+            $art['author_id']   = 67;
+            $art['author_name'] = "Thanh Niên";
+            $art['avatar_url']  = 'public/img/avatar/thanhnien.png';
         }
         unset($art);
 
-        // 3. Sắp xếp theo created_at giảm dần
+        // 3. Gộp RSS + DB articles
+        $articles = array_merge($rssArticles1, $rssArticles2, $dbArticles);
+
+        // 4. Sắp xếp theo created_at giảm dần
         usort($articles, function ($a, $b) {
             return strtotime($b['created_at']) - strtotime($a['created_at']);
         });
 
-        // 4. Load view Home
+        // 5. Load view Home
         ob_start();
         require_once 'view/page/Home.php';
         $content = ob_get_clean();
 
-        // 5. Load layout chính
+        // 6. Load layout chính
         $profile = false;
         require_once 'view/layout/main.php';
     }
+
 
 
     public static function profile_business()
