aa2050ed45eec31f9c3898d2fb6372c14b238a12
RedCode <nguyenquangtrung13062005@gmail.com>
2025-09-16 14:17:32 +0700

    có giao diện event chi tiết

---
+++ view/page/Event.php
@@ -0,0 +1,51 @@
+<main class="main-content">
+
+        <div class="content-left">
+
+            <div class="block-k">
+                <div class="detail">
+                    <h1> D11: Ngày GDKHQ trả cổ tức năm 2021 bằng cổ phiếu (100:12)</h1>
+                    <div class="d-time">
+                        <span>13 ngày</span> 
+                        
+                    </div>
+                    <div class="line"></div>
+
+                    <article>
+
+                          
+
+<p>- Ngày giao dịch không hưởng quyền: 05/9/2025</p> 
+<p>- Ngày đăng ký cuối cùng: 08/09/2025</p>
+<div class="ads_middle">
+ <div id="adsWeb_AdsArticleMiddle" class="banner fyi" data-position="Web_AdsArticleMiddle" style="display: none;"> 
+  <script>
+window.addEventListener('load', function(){ if(typeof Web_AdsArticleMiddle != 'undefined'){window.CMS_BANNER.pushAds(Web_AdsArticleMiddle, 'adsWeb_AdsArticleMiddle');}else{document.getElementById('adsWeb_AdsArticleMiddle').style.display = "none";} });
+   </script> 
+ </div>
+</div> 
+<p>- Lý do mục đích: Trả cổ tức năm 2021 bằng cổ phiếu</p> 
+<p>- Tỷ lệ thực hiện: 12% (Cổ đông sở hữu 100 cổ phiếu được nhận 12 cổ phiếu mới).</p>
+
+                     
+                    </article>
+                    <input type="hidden" id="hdd_id" value="13975">
+
+                    <div data-id="13975" data-type="1" class="box-n-sc">
+
+                       
+                    </div>
+
+                   
+                   
+                </div>
+                
+            </div>
+                    
+
+            <div class="detail-more cover-page">
+
+            </div>
+        </div>
+
+    </main>
