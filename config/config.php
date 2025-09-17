596dfcf7401b56ae1413d45b22110e9bfc63f816
RedCode <nguyenquangtrung13062005@gmail.com>
2025-09-17 12:09:23 +0700

    thêm cấu hình chọn (mặc định cả server với  dự án mình đang làm điều trỏ về aiven)

--- config/config.php
+++ config/config.php
@@ -2,10 +2,10 @@
 
 
 // Database Configuration
-$useConfig1 = true; // true dùng config 1, false dùng config 2
+$useConfig1 = false; // true dùng config 1, false dùng config 2
 
 if ($useConfig1) {
-    // Cấu hình 1
+    // Cấu hình 1 triển khai cho server nhưng database trỏ tại server
     define('DB_HOST', 'localhost');
     define('DB_USER', 'nguonnh1_tmi_mxh');
     define('DB_PASS', '9@hvPBN9yZrXwjH}');
@@ -15,15 +15,22 @@ if ($useConfig1) {
     define('SITE_NAME', 'MXH.ORG.VN');
     define('SITE_DESCRIPTION', 'Diễn đàn Tài chính Việt Nam');
 } else {
-    // Cấu hình 2
+    // Cấu hình 2 triển khai cho server hoặc local nhưng trỏ database aiven
     define('DB_HOST', 'mysql-37b8db9-nguyenquangtrung13062005-8a5b.d.aivencloud.com');
     define('DB_USER', 'avnadmin');
     define('DB_PASS', 'AVNS_brCrqfhHuYX88MkBMqS');
     define('DB_NAME', 'dff_db');
     define('DB_PORT', '27925');
-    define('BASE_URL', 'http://localhost/DFF.VN');
-    define('SITE_NAME', 'DFF.VN');
-    define('SITE_DESCRIPTION', 'Diễn đàn Tài chính Việt Nam');
+    $use="local"; // mặc định local
+    if ($use == "server") {
+        define('BASE_URL', 'https://mxh.org.vn');
+        define('SITE_NAME', 'MXH.ORG.VN');
+        define('SITE_DESCRIPTION', 'Diễn đàn Tài chính Việt Nam');
+    } else {
+        define('BASE_URL', 'http://localhost/DFF.VN');
+        define('SITE_NAME', 'DFF.VN');
+        define('SITE_DESCRIPTION', 'Diễn đàn Tài chính Việt Nam');
+    }
 }
 
 // Google OAuth2
