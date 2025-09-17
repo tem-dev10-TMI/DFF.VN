7c28883c6e8ec978797c915d25c6c071c5486ab8
Le Tan Dat <tandat0375381200@gmail.com>
2025-09-17 17:23:25 +0700

    sửa admin xíu

--- view/admin/views/layout/footer.php
+++ view/admin/views/layout/footer.php
@@ -1,7 +1,8 @@
 <?php if (!empty($_SESSION['user'])): ?>
   <span class="navbar-text me-3">
-    Xin chào, 
+    Xin chào,
     <?= e($_SESSION['user']['name'] ?? $_SESSION['user']['username']) ?>
   </span>
   <a class="btn btn-outline-light btn-sm" href="<?= BASE_URL ?>/admin.php?route=logout">Đăng xuất</a>
 <?php endif; ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
