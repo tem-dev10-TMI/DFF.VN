cfd59ec5733bf8a56dbdd9892117a3584f529376
Le Tan Dat <tandat0375381200@gmail.com>
2025-09-17 09:15:39 +0700

    chỉnh sửa NewsController.php

--- controller/NewsController.php
+++ controller/NewsController.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'model/newsmodel.php';
+require_once __DIR__ . '/../model/NewsModel.php';
 
 class NewsController
 {
@@ -7,30 +7,30 @@ class NewsController
     {
         // Lấy danh sách bài viết
         $articles = NewsModel::getAllArticles();
-       $articles = NewsModel::getLatestArticles($limit = 6);
+        $articles = NewsModel::getLatestArticles($limit = 6);
 
         // Load view
         ob_start();
-        require_once 'view/page/News.php';
+        require_once __DIR__ . '/../view/page/News.php';
         $content = ob_get_clean();
 
         // Load layout
         $profile = false;
-        require_once 'view/layout/main.php';
+        require_once __DIR__ . '/../view/layout/main.php';
     }
 
     public static function detail($id)
     {
         // Lấy 1 bài viết chi tiết
-       $article   = NewsModel::getArticleById($id);
+        $article   = NewsModel::getArticleById($id);
 
         // Load view
         ob_start();
-        require_once 'view/news/detail.php';
+        require_once __DIR__ . '/../view/news/detail.php';
         $content = ob_get_clean();
 
         // Load layout
         $profile = false;
-        require_once 'view/layout/main.php';
+        require_once __DIR__ . '/../view/layout/main.php';
     }
 }
