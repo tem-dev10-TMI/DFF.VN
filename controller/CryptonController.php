3424fb51716f771cc587979347e57f17c2370f3a
Le Tan Dat <tandat0375381200@gmail.com>
2025-09-17 09:03:29 +0700

    fix file CryptonController.php

--- controller/CryptonController.php
+++ controller/CryptonController.php
@@ -1,13 +1,15 @@
 <?php
-require_once "model/MarketModel.php";
+require_once __DIR__ . "/../model/MarketModel.php";
 
-class CryptonController {
-    public function index() {
+class CryptonController
+{
+    public function index()
+    {
         // Lấy dữ liệu từ model
         $markets = MarketModel::getMarkets();
         $currentDate = date("d/m/Y H:i");
 
         // Gửi qua view
-        require_once "view/page/Crypton.php";
+        require_once __DIR__ . "/../view/page/Crypton.php";
     }
 }
