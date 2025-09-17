b3585e7c5822f34a65ab014f718477f6c36d3f7d
RedCode <nguyenquangtrung13062005@gmail.com>
2025-09-17 11:52:58 +0700

    đã thêm cấu hình chọn database

--- config/config.php
+++ config/config.php
@@ -1,17 +1,30 @@
 <?php
+
+
 // Database Configuration
-define('DB_HOST', 'localhost');
-define('DB_USER', 'nguonnh1_tmi_mxh');
-define('DB_PASS', '9@hvPBN9yZrXwjH}');
-define('DB_NAME', 'nguonnh1_tmi_mxh');
-define('DB_PORT', '3306');
-define('BASE_URL', 'http://mxh.org.vn');
-// Site Configuration
-define('SITE_NAME', 'MXH.ORG.VN');
-define('SITE_DESCRIPTION', 'Diễn đàn Tài chính Việt Nam');
+$useConfig1 = true; // true dùng config 1, false dùng config 2
 
-
-
+if ($useConfig1) {
+    // Cấu hình 1
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'nguonnh1_tmi_mxh');
+    define('DB_PASS', '9@hvPBN9yZrXwjH}');
+    define('DB_NAME', 'nguonnh1_tmi_mxh');
+    define('DB_PORT', '3306');
+    define('BASE_URL', 'https://mxh.org.vn');
+    define('SITE_NAME', 'MXH.ORG.VN');
+    define('SITE_DESCRIPTION', 'Diễn đàn Tài chính Việt Nam');
+} else {
+    // Cấu hình 2
+    define('DB_HOST', 'mysql-37b8db9-nguyenquangtrung13062005-8a5b.d.aivencloud.com');
+    define('DB_USER', 'avnadmin');
+    define('DB_PASS', 'AVNS_brCrqfhHuYX88MkBMqS');
+    define('DB_NAME', 'dff_db');
+    define('DB_PORT', '27925');
+    define('BASE_URL', 'http://localhost/DFF.VN');
+    define('SITE_NAME', 'DFF.VN');
+    define('SITE_DESCRIPTION', 'Diễn đàn Tài chính Việt Nam');
+}
 
 // Google OAuth2
 define('GOOGLE_CLIENT_ID', '883631790996-ivg36caiogkoqbaptil3k9l7jumg1aif.apps.googleusercontent.com');
