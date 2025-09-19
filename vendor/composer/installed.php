441eeff5d5dd90623962ac7a886479a8674062aa
RedCode <nguyenquangtrung13062005@gmail.com>
2025-09-20 00:35:42 +0700

    file of compposer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '34cf4c245065473d43614c49c99649799e66592b',
+        'reference' => '60a36d5c1f0e0811e636709f275ee13d25ddd9ed',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '34cf4c245065473d43614c49c99649799e66592b',
+            'reference' => '60a36d5c1f0e0811e636709f275ee13d25ddd9ed',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
