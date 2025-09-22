d711a0909e1d285b0447222c4aad7aab8969cdcc
RedCode <nguyenquangtrung13062005@gmail.com>
2025-09-22 14:52:59 +0700

    đổi màu comment global và fix cái thanh cuộn ngang

--- view/layout/sidebarLeft.php
+++ view/layout/sidebarLeft.php
@@ -1,4 +1,4 @@
-<section class="left-sidebar sticky-top sticky-offset  vh-100 overflow-auto">
+<section class="left-sidebar">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <div class="block-k left-menu bg-transparent border0 p-l-0">
--- view/page/Home.php
+++ view/page/Home.php
@@ -445,8 +445,8 @@ $comments = CommentGlobalModel::getRootCommentsPaged(20, 0);
 
 
         <div class="content-right">
-            <div class="block-k cover-chat">
-                <h5><i class="fas fa-comments"></i> Hi! TMI - DEV K25</h5>
+            <div class="block-k cover-chat ">
+                <h5 class="bg-success"><i class="fas fa-comments"></i> Hi! TMI - DEV K25</h5>
                 <ul class="list_comment">
                     <?php foreach ($comments as $c): ?>
                         <li class="chat-item" data-id="<?= $c['id'] ?>">
